1fed5fb45b48edf84b752e47c51d40315141e056
Hoang Trinh <hoang.trinhj@gmail.com>
2022-01-07 15:58:15 +0700

    feat: Track Events From AggregationExecutor

---
+++ src/router/executor.ts
@@ -0,0 +1,20 @@
+import {ExecutorExchange} from "../types/router/schema";
+import {Exchange} from "../types/router/Executor/Executor";
+
+export function handleExecutorExchange(event: Exchange): void {
+  let id = event.transaction.hash
+    .toHex()
+    .concat("-")
+    .concat(event.logIndex.toString());
+
+  let executorExchange = new ExecutorExchange(id);
+  executorExchange.executor = event.address;
+  executorExchange.pair = event.params.pair;
+  executorExchange.token = event.params.output;
+  executorExchange.amount = event.params.amountOut.toBigDecimal();
+  executorExchange.userAddress = event.transaction.from;
+  executorExchange.tx = event.transaction.hash;
+  executorExchange.blockNumber = event.block.number;
+  executorExchange.time = event.block.timestamp;
+  executorExchange.save();
+}
