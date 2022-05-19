956d27391ad7298dbbb07fd4b0a354b4f8944911
Hoang Trinh <hoang.trinhj@gmail.com>
2022-05-19 14:02:29 +0700

    feat: Track client data

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,5 +1,6 @@
 import {AggregationExecutor, RouterExchange, RouterSwapped} from "../types/router/schema";
 import {Exchange, Swapped} from "../types/router/Router/Router";
+import {ClientData} from "../types/router/RouterV3/RouterV3";
 import {Executor as AggregationExecutorTemplate} from "../types/router/templates";
 
 export function handleExchange(event: Exchange): void {
@@ -33,16 +34,42 @@ export function handleSwapped(event: Swapped): void {
     .toHex()
     .concat("-")
     .concat(event.logIndex.toString());
-  let routerSwapped = new RouterSwapped(id)
+  let routerSwapped = createOrLoadRouterSwapped(id);
   routerSwapped.router = event.address;
-  routerSwapped.pair = event.params.srcToken.toHex().concat("_").concat(event.params.dstToken.toHex())
+  routerSwapped.pair = event.params.srcToken
+    .toHex()
+    .concat("_")
+    .concat(event.params.dstToken.toHex());
   routerSwapped.tokenIn = event.params.srcToken;
   routerSwapped.amountIn = event.params.spentAmount.toBigDecimal();
-  routerSwapped.tokenOut = event.params.dstToken
+  routerSwapped.tokenOut = event.params.dstToken;
   routerSwapped.amountOut = event.params.returnAmount.toBigDecimal();
   routerSwapped.userAddress = event.transaction.from;
   routerSwapped.tx = event.transaction.hash;
   routerSwapped.blockNumber = event.block.number;
   routerSwapped.time = event.block.timestamp;
   routerSwapped.save();
+}
+
+export function handleClientData(event: ClientData): void {
+  let id = event.transaction.hash
+    .toHex()
+    .concat("-")
+    .concat(event.logIndex.toString());
+
+  let routerSwapped = createOrLoadRouterSwapped(id);
+
+  routerSwapped.clientData = event.params.clientData;
+
+  routerSwapped.save();
+}
+
+export function createOrLoadRouterSwapped(id: string): RouterSwapped {
+  let routerSwapped = RouterSwapped.load(id);
+
+  if (routerSwapped === null) {
+    routerSwapped = new RouterSwapped(id);
+  }
+
+  return routerSwapped as RouterSwapped;
 }
