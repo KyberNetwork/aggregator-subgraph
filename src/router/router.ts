978e02f978ab47cf0857611f85559e2d5d20093a
tien7668 <33758175+tien7668@users.noreply.github.com>
2022-06-08 14:44:53 +0700

    handle event swapped (#4)
    * handle event swapped
    
    * feat: Track client data
    
    * Expose tokenIn/tokenOut for RouterExchange (#6)
    
    * Expose tokenIn/tokenOut for RouterExchange
    
    * Expose source in clientData for RouterSwapped
    
    * Fixed parse error
    
    * Fixed data lost due to inconsistent id
    
    Co-authored-by: Hoang Trinh <hoang.trinhj@gmail.com>
    Co-authored-by: Duc Le <duc.le@kyber.network>

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,6 +1,8 @@
-import {AggregationExecutor, RouterExchange} from "../types/router/schema";
-import {Exchange} from "../types/router/Router/Router";
+import {AggregationExecutor, RouterExchange, RouterSwapped} from "../types/router/schema";
+import {Exchange, Swapped} from "../types/router/Router/Router";
+import {ClientData} from "../types/router/RouterV3/RouterV3";
 import {Executor as AggregationExecutorTemplate} from "../types/router/templates";
+import {JSON} from 'assemblyscript-json';
 
 export function handleExchange(event: Exchange): void {
   let executor = AggregationExecutor.load(event.params.pair.toHex());
@@ -12,11 +14,9 @@ export function handleExchange(event: Exchange): void {
   }
 
   let id = event.transaction.hash
-    .toHex()
-    .concat("-")
-    .concat(event.logIndex.toString());
+    .toHex();
 
-  let routerExchange = new RouterExchange(id);
+  let routerExchange = createOrLoadRouterExchange(id);
   routerExchange.router = event.address;
   routerExchange.pair = event.params.pair;
   routerExchange.token = event.params.output;
@@ -26,4 +26,73 @@ export function handleExchange(event: Exchange): void {
   routerExchange.blockNumber = event.block.number;
   routerExchange.time = event.block.timestamp;
   routerExchange.save();
+}
+
+export function handleSwapped(event: Swapped): void {
+  let id = event.transaction.hash
+      .toHex();
+  let routerSwapped = createOrLoadRouterSwapped(id);
+  routerSwapped.router = event.address;
+  routerSwapped.pair = event.params.srcToken
+    .toHex()
+    .concat("_")
+    .concat(event.params.dstToken.toHex());
+  routerSwapped.tokenIn = event.params.srcToken;
+  routerSwapped.amountIn = event.params.spentAmount.toBigDecimal();
+  routerSwapped.tokenOut = event.params.dstToken;
+  routerSwapped.amountOut = event.params.returnAmount.toBigDecimal();
+  routerSwapped.userAddress = event.transaction.from;
+  routerSwapped.tx = event.transaction.hash;
+  routerSwapped.blockNumber = event.block.number;
+  routerSwapped.time = event.block.timestamp;
+  routerSwapped.save();
+
+  let routerExchange = createOrLoadRouterExchange(id);
+  routerExchange.tokenIn = event.params.srcToken;
+  routerExchange.tokenOut = event.params.dstToken;
+  routerExchange.save();
+}
+
+export function handleClientData(event: ClientData): void {
+  let id = event.transaction.hash
+      .toHex();
+
+  let routerSwapped = createOrLoadRouterSwapped(id);
+
+  routerSwapped.clientData = event.params.clientData;
+  let rawStr = event.params.clientData.toString();
+  let indexOfSource = rawStr.indexOf('source');
+  if (indexOfSource > 2) {
+    let data = rawStr.substring(rawStr.indexOf('source') - 2);
+    let clientData = <JSON.Obj>(JSON.parse(data))
+
+    if (clientData != null && clientData.has("source")) {
+      let source = clientData.getString("source");
+      routerSwapped.source = source != null ? source.valueOf() : data;
+    } else {
+      routerSwapped.source = data;
+    }
+  }
+
+  routerSwapped.save();
+}
+
+export function createOrLoadRouterSwapped(id: string): RouterSwapped {
+  let routerSwapped = RouterSwapped.load(id);
+
+  if (routerSwapped === null) {
+    routerSwapped = new RouterSwapped(id);
+  }
+
+  return routerSwapped as RouterSwapped;
+}
+
+export function createOrLoadRouterExchange(id: string): RouterExchange {
+  let routerExchange = RouterExchange.load(id);
+
+  if (routerExchange == null) {
+    routerExchange = new RouterExchange(id);
+  }
+
+  return routerExchange as RouterExchange;
 }
